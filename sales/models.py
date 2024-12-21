f21fa8dc05f4b1657338ba394e566ef39338b898
phischa <ph.git@mailbox.org>
2024-12-21 16:19:05 +0100

    customer model

--- sales/models.py
+++ sales/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Customer(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    # newsletter_abo = models.BooleanField()
+    # email_address = models.EmailField()
+    # account = models.FloatField()
