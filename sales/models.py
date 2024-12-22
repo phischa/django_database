ba4cf41475aa25b09a9329280786204a6809a0c7
phischa <ph.git@mailbox.org>
2024-12-22 15:50:46 +0100

    added chart fields

--- sales/models.py
+++ sales/models.py
@@ -5,6 +5,6 @@ from django.db import models
 class Customer(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    # newsletter_abo = models.BooleanField()
-    # email_address = models.EmailField()
-    # account = models.FloatField()
+    newsletter_abo = models.BooleanField(default=False)
+    email_address = models.EmailField(max_length=30, blank=True, default="")
+    account = models.FloatField(blank=True, null=True)
