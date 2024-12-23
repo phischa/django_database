ecdf08f5259065d4ae70ec779bdd13e85e063b36
phischa <ph.git@mailbox.org>
2024-12-23 19:26:22 +0100

    admin panel changes

--- sales/admin.py
+++ sales/admin.py
@@ -4,8 +4,28 @@ from .models import Customer, Product, Bill, Order, ProductType
 # Register your models here.
 
 class CustomerAdmin(admin.ModelAdmin):
-    list_filter=["first_name", "last_name"]
-    list_display=["first_name", "last_name", "account"]
+    list_filter = ["first_name", "last_name"]
+    list_display = ["first_name", "last_name", "account"]
+
+    readonly_fields = []
+
+    prepopulated_fields = {"slug":["first_name", "last_name"]}
+
+    fieldsets = [
+        (
+            None,
+            {
+                "fields": ["first_name", "last_name", "account"],
+            },
+        ),
+        (
+            "Advanced options",
+            {
+                "classes": ["collapse"],
+                "fields": ["newsletter_abo", "slug"],
+            },
+        ),
+    ]
 
 
 admin.site.register(Customer, CustomerAdmin)
