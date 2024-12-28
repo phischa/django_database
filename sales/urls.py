847eecf0faac3e084cc7b17973bedd62a3beaae9
phischa <ph.git@mailbox.org>
2024-12-28 13:33:07 +0100

    detail view

--- sales/urls.py
+++ sales/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CustomerListView, CustomerListSearchView
+from .views import CustomerListView, CustomerListSearchView, CustomerDetailView
 
 urlpatterns = [
     path('', CustomerListView.as_view()),#
-    path('<str:name>/', CustomerListSearchView.as_view())
+    path('<str:name>/', CustomerListSearchView.as_view()),
+    path('customer/<int:pk>/', CustomerDetailView.as_view()),
 ]
--- sales/views.py
+++ sales/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Customer, Bill
 
 # Create your views here.
@@ -14,3 +16,13 @@ class CustomerListSearchView(CustomerListView):
     def get_queryset(self):
         name = self.kwargs.get("name")
         return Customer.objects.filter(first_name=name)
+    
+
+class CustomerDetailView(DetailView):
+    model = Customer
+    template_name = "sales/detail.html"
+
+    def get_object(self):
+        obj = super().get_object()
+        obj.last_accessed = timezone.now()
+        return obj
